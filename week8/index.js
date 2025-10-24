793e6d4a0fffb620e2a8b96c972006520907dbb0
ADMIN <ADMIN@NGQUYKHANH3011>
2025-10-24 17:05:03 +0700

    uh ill check later

--- week8/index.js
+++ week8/index.js
@@ -6,10 +6,10 @@ const app = Vue.createApp({
 app.component('khanhnq', {
     data() {
         return {
-            message: 'Hello, welcome',
+            message: 'Loading..',
             posts: [
-                {title: 'post 1', body: 'content 1'},
-                {title: 'post 2', body: 'content 2'}
+                // {title: 'post 1', body: 'content 1'},
+                // {title: 'post 2', body: 'content 2'}
         ]
         }
     },
@@ -28,13 +28,73 @@ app.component('khanhnq', {
             'https://jsonplaceholder.typicode.com/posts',
             data => {
                 this.posts = data;
-                this.message = '';
+                this.message = 'where the heck is my message im not blind';
             }
-        );
+        )//.fail (
+            //(err) => {
+                //this.message="This is error"
+            //}
+        //)
 
-    },
+        fetch ('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            this.posts = data;
+            this.message = '';
+        }).catch(err =>{
+            this.message = "Failed";
+        });
+
+    }
 
     
 });
 
+app.component('yolo', {
+    data() {
+        return {
+            title: '',
+            body: ''
+        }
+    },
+    template: `
+    {{message}}
+    <form @submit="handleSubmit">
+        <div>
+            <label for="title">Title</label>
+            <input type="text" v-model="title" id="title">
+        </div>
+
+        <div>
+            <label for="body">Body</label>
+            <input type="text" v-model="body" id="body">
+        </div>
+        <button>Submit</button>
+    </form>`,
+
+    method: {
+        handleSubmit(event) {
+            event.preventDefault(); //stop redirect to action url -he said
+            
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                data: JSON.stringify({
+                    title: this.title,
+                    body: this.body, 
+                    userID: 1
+                }),
+                header: {
+                    'Content-Type' : 'application/json'
+                }
+            }). then(response => {
+                return response.json();
+            }). then(data =>  {
+                this.message = 'create successfully'
+            }).catch(error => 
+                this.message = 'error'
+            )
+        }
+    }
+})
 app.mount('#app')
